2f6a2f3758be67d0e98ac5645d51eb6baa62e4f6
gabrielfelice <gabifelice@gmail.com>
2026-01-20 19:34:15 +0000

    página em construção + história do Mestre Chu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-  return <h1>Te amo Rita Carolina, minha alegria</h1>;
+  return (
+    <h1>
+      PÁGINA EM CONSTRUÇÃO Mas você já conhece a história do Mestre Chu? Reza a
+      lenda que existia, no Japão, um mestre chamado Mestre Chu, com quem um
+      menino sempre se consultava quando tinha algum questionamento a respeito
+      da vida. Um belo dia, o menino acordou com uma pergunta. Uma pergunta que
+      ele precisava muito fazer pro Mestre Chu, pois ele certamente saberia
+      responder. Então o menino vai ao encontro do seu sábio Mestre e faz a sua
+      pergunta, apreensivo, mas determinado. — Mestre Chu! Por que... POR QUE AS
+      PESSOAS FALAM QUE OS JAPONESES SÃO TODOS IGUAIS? Eis que ao ser indagado
+      pelo menino, levanta e lhe diz: — Eu não sou o Mestre Chu. Fim
+    </h1>
+  );
 }
 
 export default Home;
